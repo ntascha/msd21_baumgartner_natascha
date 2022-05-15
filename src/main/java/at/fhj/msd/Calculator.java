e297e4e2aef0c1bf723d3c86e63777d02b92f7f1
Natascha Baumgartner <natascha.baumgartner@edu-fh.joanneum.at>
2022-05-15 18:52:34 +0200

    Calculator.java

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -2,23 +2,36 @@ package at.fhj.msd;
 
 public class Calculator {
 
-    public double add(double number1, double number2){
-        double additionResult = number1+number2;
+    public double add(double number1, double number2) {
+        double additionResult = number1 + number2;
         return additionResult;
     }
 
-    public double minus(double number1, double number2){
-        double subResult = number1-number2;
+    public double minus(double number1, double number2) {
+        double subResult = number1 - number2;
         return subResult;
     }
 
-    public double divide(double number1, double number2){
-        double divResult = number1/number2;
+    public double divide(double number1, double number2) {
+        double divResult = number1 / number2;
         return divResult;
     }
 
-    public double multiply(double number1, double number2){
-        double multResult = number1*number2;
+    public double multiply(double number1, double number2) {
+        double multResult = number1 * number2;
         return multResult;
     }
+
+    public double faculty(int number) {
+//    if (number < 1) {
+//        return 0;
+//    }
+//    int temp = 1;
+//    for (int i = 2; i <= number; i++) {
+//        temp = temp * i;
+//    }
+//    return temp;
+        return 0;
+    }
 }
+
