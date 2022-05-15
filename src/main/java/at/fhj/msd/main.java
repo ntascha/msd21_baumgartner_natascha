0771c3b7e51c2887d97784306a3da1ae2c32e594
Natascha Baumgartner <natascha.baumgartner@edu-fh.joanneum.at>
2022-05-15 20:00:13 +0200

    main.java

--- src/main/java/at/fhj/msd/main.java
+++ src/main/java/at/fhj/msd/main.java
@@ -1,15 +1,20 @@
 package at.fhj.msd;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class main {
 
     public static void main(String[] args) {
 
     Calculator Calculator = new Calculator();
 
-        System.out.println(Calculator.add(1, 2));
-        System.out.println(Calculator.minus(1, 3));
-        System.out.println(Calculator.divide(5, 2.5));
-        System.out.println(Calculator.multiply(1.3, 1.0));
+        System.out.println(Calculator.add(5, 3));
+        System.out.println(Calculator.minus(5, 3));
+        System.out.println(Calculator.multiply(5, 3));
+        System.out.println(Calculator.divide(4, 2));
+        System.out.println(Calculator.faculty(5));
+        System.out.println(Calculator.faculty(5));
+        System.out.println(Calculator.faculty(-5));
     }
 
 }
